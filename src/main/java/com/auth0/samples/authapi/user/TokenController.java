f9a60dd665288add24d29f9177fa55e8fecb926e
aalekseyev <aalekseyev@localhost>
2017-09-11 18:15:27 +0300

    added LOGGER to TokenController

--- src/main/java/com/auth0/samples/authapi/user/TokenController.java
+++ src/main/java/com/auth0/samples/authapi/user/TokenController.java
@@ -1,5 +1,7 @@
 package com.auth0.samples.authapi.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,8 @@ import com.auth0.samples.authapi.user.dto.ApplicationUserDto;
 @RestController
 @RequestMapping("/token")
 public class TokenController {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(TokenController.class);
 
     private AuthenticationManager authenticationManager;
     
@@ -80,12 +84,14 @@ public class TokenController {
     		try {
     		    newRefreshToken = refreshToken.refresh();
     		} catch (RuntimeException e) {
+    		    LOG.error("Failed to refresh token: {}", e, refreshToken.getTokenId());
     		    return new ResponseEntity<>(HttpStatus.FORBIDDEN);
     		}
     		
     		TokensDto result = new TokensDto(jwt.toString(), newRefreshToken.getTokenId(), csrfToken.toString());
     		return new ResponseEntity<>(result, HttpStatus.OK);
     	} else {
+    	    LOG.error("Unable to find refresh token: {}", refreshTokenDto.getTokenId());
     	    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     	}
     }
